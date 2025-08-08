47e62747bb49995f0f6cdb32f1e4bb51044b0630
Alex Ricardo Martins <amartins2@byupathway.edu>
2025-08-07 19:38:27 -0600

    new styling and adding images

--- Final Project/scripts/footer.js
+++ Final Project/scripts/footer.js
@@ -3,3 +3,12 @@ const yearSpan = document.getElementById("year");
 if (yearSpan) {
     yearSpan.textContent = new Date().getFullYear();
 }
+
+document.getElementById("searchForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const query = document.getElementById("searchInput").value.trim();
+    if (query) {
+        alert("You searched for: " + query);
+        // Optional: Redirect or filter cookies
+    }
+});
